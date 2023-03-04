1937f09dbeaf73a45133f14f5bca8043ac25962d
Kavan <kavanvavadiya24@gmail.com>
2023-03-04 23:25:10 +0530

    inital frontend

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Home from "./pages/home/Home";
+// import Login from "./pages/login/Login";
+// import Register from "./pages/register/Register";
+// import Profile from "./pages/profile/Profile";
+
+function App() {
+  return (
+    <>
+   <Home />
+   </>
+  );
+}
+
+export default App;
