2f0961cfc7c2b59735007f280a81d13337b438c2
Kavan <kavanvavadiya24@gmail.com>
2023-03-09 22:56:30 +0530

    frontend + backend

--- Server/index.js
+++ Server/index.js
@@ -8,6 +8,9 @@ const app = express();
 dotenv.config()
 // const url = process.env.MONGO_URL
 
+const multer  = require('multer')
+const path = require("path");
+
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
@@ -18,10 +21,31 @@ mongoose.connect(url,(err)=>{
     console.log("mongoDB connected")
 })
   
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
   //middleware
   app.use(express.json());
   app.use(helmet());
   app.use(morgan("common"));
+
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+      // cb(null, file.originalname);
+      cb(null, req.body.name);
+    },
+  });
+
+  const upload = multer({ storage: storage });
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    try {
+      return res.status(200).json("File uploded successfully");
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
   //routes
   app.use("/api/auth", authRoute);
--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import Home from "./pages/home/Home";
-// import Login from "./pages/login/Login";
-// import Register from "./pages/register/Register";
-// import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Profile from "./pages/profile/Profile";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const { user } = useContext(AuthContext);
   return (
-    <>
-   <Home />
-   </>
+     <Router>
+      <Routes>
+        {/* <Route exact path="/" element={<Home />}></Route> */}
+        <Route exact path="/" element={user ? <Home /> : <Register />}></Route>
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />}></Route>     
+        <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} ></Route>
+        <Route path="/profile/:username" element={<Profile />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
